f93e770db2d81c4c593bcef756530bcc3be92851
Mosesomo <moseswamboga4@gmail.com>
2024-11-23 14:37:13 +0300

    added some underline effects on hover to the links

--- script.js
+++ script.js
@@ -54,3 +54,29 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+// Update the active Class dynamically
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+            if (window.scrollY >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove('active');
+            if (link.getAttribute('href').includes(current)) {
+                link.classList.add('active');
+            }
+        });
+    });
+});
